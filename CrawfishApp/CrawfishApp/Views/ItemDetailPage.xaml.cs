61d7e15086529129334d8ac84c05bad7bcdc581c
tbcrawfish <tylerballcrawford@gmail.com>
2022-02-03 18:41:19 -0800

    New App
    Started new xamarin app

---
+++ CrawfishApp/CrawfishApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using CrawfishApp.ViewModels;
+
+namespace CrawfishApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ CrawfishApp/CrawfishApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using CrawfishApp.Models;
+using CrawfishApp.ViewModels;
+
+namespace CrawfishApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
